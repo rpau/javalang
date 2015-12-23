fe46405fb37d436a9d47e422279c3451b52ea67a
rpau <rpau@ac.upc.edu>
2015-12-23 18:10:12 +0100

    field parsing from grammar

--- src/main/java/org/walkmod/javalang/javadoclinks/JavadocLinkParserTokenManager.java
+++ src/main/java/org/walkmod/javalang/javadoclinks/JavadocLinkParserTokenManager.java
@@ -5,6 +5,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.List;
 import java.util.LinkedList;
+import java.io.ByteArrayInputStream;
 
 /** Token Manager. */
 public class JavadocLinkParserTokenManager implements JavadocLinkParserConstants
