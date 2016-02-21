9b70080fa70c9b49c9fd3e0de473f3c76fbc222e
rpau <rpau@ac.upc.edu>
2016-02-21 08:18:03 +0100

    format

--- src/test/java/org/walkmod/javalang/ast/ASTParserTest.java
+++ src/test/java/org/walkmod/javalang/ast/ASTParserTest.java
@@ -28,144 +28,134 @@ import org.walkmod.javalang.ast.stmt.ReturnStmt;
 
 public class ASTParserTest {
 
-	@Test
-	public void testParsing() throws Exception {
-		File f = new File("src/test/resources/source1.8.txt");
-		CompilationUnit cu = ASTManager.parse(f);
-		Assert.assertNotNull(cu);
-		System.out.println(cu.toString());
-	}
+   @Test
+   public void testParsing() throws Exception {
+      File f = new File("src/test/resources/source1.8.txt");
+      CompilationUnit cu = ASTManager.parse(f);
+      Assert.assertNotNull(cu);
+      System.out.println(cu.toString());
+   }
 
-	@Test
-	public void testLambdaParsing() throws Exception {
-		File f = new File("src/test/resources/lambda.txt");
-		CompilationUnit cu = ASTManager.parse(f);
-		Assert.assertNotNull(cu);
-		System.out.println(cu.toString());
-	}
+   @Test
+   public void testLambdaParsing() throws Exception {
+      File f = new File("src/test/resources/lambda.txt");
+      CompilationUnit cu = ASTManager.parse(f);
+      Assert.assertNotNull(cu);
+      System.out.println(cu.toString());
+   }
 
-	
-	@Test
-	public void testLicense() throws Exception {
-		File f = new File("src/test/resources/licensed-sources.txt");
-		CompilationUnit cu = ASTManager.parse(f);
-		Assert.assertNotNull(cu);
-		cu.toString();
-		// System.out.println(cu.toString());
-	}
+   @Test
+   public void testLicense() throws Exception {
+      File f = new File("src/test/resources/licensed-sources.txt");
+      CompilationUnit cu = ASTManager.parse(f);
+      Assert.assertNotNull(cu);
+      cu.toString();
+      // System.out.println(cu.toString());
+   }
 
-	@Test
-	public void testLinesBetweenStmts() throws Exception {
-		File f = new File("src/test/resources/multipleLinesBetweenStmt.txt");
-		CompilationUnit cu = ASTManager.parse(f);
-		Assert.assertNotNull(cu);
-		System.out.println(cu.toString());
-	}
+   @Test
+   public void testLinesBetweenStmts() throws Exception {
+      File f = new File("src/test/resources/multipleLinesBetweenStmt.txt");
+      CompilationUnit cu = ASTManager.parse(f);
+      Assert.assertNotNull(cu);
+      System.out.println(cu.toString());
+   }
 
-	@Test
-	public void testLinesBetweenImports() throws Exception {
-		File f = new File("src/test/resources/multipleLinesBetweenImports.txt");
-		CompilationUnit cu = ASTManager.parse(f);
-		Assert.assertNotNull(cu);
-		System.out.println(cu.toString());
-	}
+   @Test
+   public void testLinesBetweenImports() throws Exception {
+      File f = new File("src/test/resources/multipleLinesBetweenImports.txt");
+      CompilationUnit cu = ASTManager.parse(f);
+      Assert.assertNotNull(cu);
+      System.out.println(cu.toString());
+   }
 
-	@Test
-	public void testLinesBetweenParameters() throws Exception {
-		File f = new File(
-				"src/test/resources/multipleLinesBetweenParameters.txt");
-		CompilationUnit cu = ASTManager.parse(f);
-		Assert.assertNotNull(cu);
-		System.out.println(cu.toString());
-	}
+   @Test
+   public void testLinesBetweenParameters() throws Exception {
+      File f = new File("src/test/resources/multipleLinesBetweenParameters.txt");
+      CompilationUnit cu = ASTManager.parse(f);
+      Assert.assertNotNull(cu);
+      System.out.println(cu.toString());
+   }
 
-	@Test
-	public void testLines() throws Exception {
-		File f = new File("src/test/resources/test.txt");
-		CompilationUnit cu = ASTManager.parse(f);
-		Assert.assertNotNull(cu);
-		System.out.println(cu.toString());
-	}
+   @Test
+   public void testLines() throws Exception {
+      File f = new File("src/test/resources/test.txt");
+      CompilationUnit cu = ASTManager.parse(f);
+      Assert.assertNotNull(cu);
+      System.out.println(cu.toString());
+   }
 
-	@Test
-	public void testMultiplePrintsInComments() throws Exception {
-		File f = new File("src/test/resources/comments.txt");
-		CompilationUnit cu = ASTManager.parse(f);
-		Assert.assertNotNull(cu);
-		System.out.println(cu.toString());
-		System.out.println(cu.toString());
-	}
+   @Test
+   public void testMultiplePrintsInComments() throws Exception {
+      File f = new File("src/test/resources/comments.txt");
+      CompilationUnit cu = ASTManager.parse(f);
+      Assert.assertNotNull(cu);
+      System.out.println(cu.toString());
+      System.out.println(cu.toString());
+   }
 
-	@Test
-	public void testCommentsInsideNode() throws Exception {
-		File f = new File("src/test/resources/comments.txt");
-		CompilationUnit cu = ASTManager.parse(f);
-		Assert.assertNotNull(cu);
+   @Test
+   public void testCommentsInsideNode() throws Exception {
+      File f = new File("src/test/resources/comments.txt");
+      CompilationUnit cu = ASTManager.parse(f);
+      Assert.assertNotNull(cu);
 
-		System.out.println(cu.getTypes().get(0).getMembers().get(0).toString());
-	}
-	
-	@Test
-	public void testLambdaCast() throws Exception {
-		File f = new File(
-				"src/test/resources/lambdacast-error.txt");
-		CompilationUnit cu = ASTManager.parse(f);
-		Assert.assertNotNull(cu);
-		System.out.println(cu.toString());
-	}
-	
-	@Test
-	public void testConditionalLambda() throws Exception{
-		File f = new File(
-				"src/test/resources/lambda-failure-conditional.txt");
-		CompilationUnit cu = ASTManager.parse(f);
-		Assert.assertNotNull(cu);
-		System.out.println(cu.toString());
-	}
-	
-	
-	@Test
-	public void testConditionalLambda2() throws Exception{
-		File f = new File(
-				"src/test/resources/lambda-failure-conditional2.txt");
-		CompilationUnit cu = ASTManager.parse(f);
-		Assert.assertNotNull(cu);
-		System.out.println(cu.toString());
-	}
-	
-	@Test
-	public void testConditionalLambda3() throws Exception{
-		File f = new File(
-				"src/test/resources/lambda-failure-conditional3.txt");
-		CompilationUnit cu = ASTManager.parse(f);
-		Assert.assertNotNull(cu);
-		System.out.println(cu.toString());
-	}
-	
-	
-	@Test
-	public void testPositionsOnMethodReferences() throws Exception{
-		File f = new File(
-				"src/test/resources/methodReferenceFailureNodePositions.txt");
-		CompilationUnit cu = ASTManager.parse(f);
-		MethodDeclaration md = (MethodDeclaration)cu.getTypes().get(0).getMembers().get(1);
-		ReturnStmt returnStmt = (ReturnStmt)md.getBody().getStmts().get(0);
-		MethodCallExpr expr = (MethodCallExpr)returnStmt.getExpr();
-		List<Expression> expressions = expr.getArgs();
-		expr = (MethodCallExpr)expressions.get(0);
-		expressions = expr.getArgs();
-		int pos = expr.getBeginColumn();
-		for(Expression arg: expressions){
-			Assert.assertTrue(arg.getBeginColumn() > pos);
-			pos = arg.getBeginColumn();
-		}
-	}
-	
-	@Test
-	public void testParseBigCommentBug() throws Exception{
-	   File f = new File(
-            "src/test/resources/bigCommentBug.txt");
+      System.out.println(cu.getTypes().get(0).getMembers().get(0).toString());
+   }
+
+   @Test
+   public void testLambdaCast() throws Exception {
+      File f = new File("src/test/resources/lambdacast-error.txt");
+      CompilationUnit cu = ASTManager.parse(f);
+      Assert.assertNotNull(cu);
+      System.out.println(cu.toString());
+   }
+
+   @Test
+   public void testConditionalLambda() throws Exception {
+      File f = new File("src/test/resources/lambda-failure-conditional.txt");
+      CompilationUnit cu = ASTManager.parse(f);
+      Assert.assertNotNull(cu);
+      System.out.println(cu.toString());
+   }
+
+   @Test
+   public void testConditionalLambda2() throws Exception {
+      File f = new File("src/test/resources/lambda-failure-conditional2.txt");
+      CompilationUnit cu = ASTManager.parse(f);
+      Assert.assertNotNull(cu);
+      System.out.println(cu.toString());
+   }
+
+   @Test
+   public void testConditionalLambda3() throws Exception {
+      File f = new File("src/test/resources/lambda-failure-conditional3.txt");
+      CompilationUnit cu = ASTManager.parse(f);
+      Assert.assertNotNull(cu);
+      System.out.println(cu.toString());
+   }
+
+   @Test
+   public void testPositionsOnMethodReferences() throws Exception {
+      File f = new File("src/test/resources/methodReferenceFailureNodePositions.txt");
+      CompilationUnit cu = ASTManager.parse(f);
+      MethodDeclaration md = (MethodDeclaration) cu.getTypes().get(0).getMembers().get(1);
+      ReturnStmt returnStmt = (ReturnStmt) md.getBody().getStmts().get(0);
+      MethodCallExpr expr = (MethodCallExpr) returnStmt.getExpr();
+      List<Expression> expressions = expr.getArgs();
+      expr = (MethodCallExpr) expressions.get(0);
+      expressions = expr.getArgs();
+      int pos = expr.getBeginColumn();
+      for (Expression arg : expressions) {
+         Assert.assertTrue(arg.getBeginColumn() > pos);
+         pos = arg.getBeginColumn();
+      }
+   }
+
+   @Test
+   public void testParseBigCommentBug() throws Exception {
+      File f = new File("src/test/resources/bigCommentBug.txt");
       CompilationUnit cu = ASTManager.parse(f);
       Assert.assertNull(cu.getImports());
-	}
+   }
 }
